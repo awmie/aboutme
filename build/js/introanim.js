c1d818ed0104652416913c3c118b93bad18a5040
awmie <amritavakole@gmail.com>
2023-09-23 22:58:25 +0530

    minor changes

--- build/js/introanim.js
+++ build/js/introanim.js
@@ -13,29 +13,32 @@
     duration: 1
   });
 
-  gsap.registerPlugin(TextPlugin);
-  gsap.fromTo('#cursor', {
-  autoAlpha: 0,
-  x: -10
-}, {
-  autoAlpha: 1,
-  duration: 1,
-  repeat: -1,
-  ease: SteppedEase.config(1)
-});
+  //hi appear
+  let hi = new SplitType('#hi');
+  let hi_char = document.querySelectorAll('.char');
 
-let textElement = document.querySelector("#text"); // Get the text element
-let cursorElement = document.querySelector("#cursor"); // Get the cursor element
-
-let tween = gsap.to(textElement, {
-  text: {
-    value: "I am a fourth-year computer science student at Gautam Buddha University in Greater Noida. My current research interests include Machine Learning, Artificial Intelligence, and Python programming."
-  },
-  duration: 10,
-  delay: 2,
-  ease: 'none',
-  onUpdate: () => {
-    // Append the cursor to the text being animated
-    textElement.appendChild(cursorElement);
+  for(j=0; j<hi_char.length; j++){
+    hi_char[j].classList.add('translate-y-32')
   }
-});
+
+  gsap.to('.char',{
+    y: 0,
+    stagger: 0.05,
+    delay: 0.02,
+    duration: 1
+    });
+
+const words = ["a Frontend Developer;", "a Python Developer;", "also interested in Machine Learning;"]
+
+let cursor = gsap.to('.cursor', {opacity:0, ease: "power2.inOut", repeat:-1})
+let masterTl = gsap.timeline({repeat: -1}).pause()
+let boxTl = gsap.timeline()
+
+boxTl
+  .to('.box', {duration:3, height:"7vw", ease: "elastic.out", onComplete: () => masterTl.play() })
+  .to('.box', {duration:2, autoAlpha:0.7, yoyo: true, repeat: -1, ease:"rough({ template: none.out, strength:  1, points: 20, taper: 'none', randomize: true, clamp: false})"})
+words.forEach(word => {
+  let tl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay:1})
+  tl.to('.text', {duration: 2, text: word})
+  masterTl.add(tl)
+})
